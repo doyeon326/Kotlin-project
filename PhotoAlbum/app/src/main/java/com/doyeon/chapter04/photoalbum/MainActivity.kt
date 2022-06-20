b74c112182d6a751f4389f5615e4ee6470152c36
doyeon326 <doyeon326@gmail.com>
2022-06-20 13:57:45 +0900

    feat: setup authorization for file storage

--- PhotoAlbum/app/src/main/java/com/doyeon/chapter04/photoalbum/MainActivity.kt
+++ PhotoAlbum/app/src/main/java/com/doyeon/chapter04/photoalbum/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.doyeon.chapter04.photoalbum
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val addPhotoButton: Button by lazy {
+        findViewById(R.id.addPhotoButton)
+    }
+
+    val startPhotoFrameModeButton: Button by lazy {
+        findViewById(R.id.startPhotoFrameModeButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initAddPhotoButton()
+        initStartPhotoFrameModeButton()
+
+    }
+
+    private fun initAddPhotoButton() {
+        addPhotoButton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    //todo 권한이 부여가 되었을 때 갤러리에서 사진 선택가능
+                } shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) ->{
+                    //todo 교육용 팝업 확인 후 권한 팝업을 띄우는 기능
+                }
+                else -> {
+                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1000)
+                }
+            }
+        }
+    }
+
+    private fun showPermissionContextPopup() {
+          AlertDialog.Builder(this)
+              .setTitle("권한이 필요합니다.")
+              .setMessage("전자액자에 앱에서 사진을 불러오 위해 권한이 필요합니다.")
+              .setPositiveButton("동의하기", { dialog, which ->
+                  requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1000)
+              })
+              .setNegativeButton("취소하기") { _, _ -> }
+              .create()
+              .show()
+    }
+
+    private fun initStartPhotoFrameModeButton() {
+
     }
 }
