77d5dc9f3ce74eb2b62fd6dc2bd6a259e4bdd2ad
doyeon326 <doyeon326@gmail.com>
2022-06-21 12:14:08 +0900

    init Pomodoro Timer

---
+++ PomodoroTimer/app/src/main/java/com/doyeon/chapter06/pomodorotimer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.doyeon.chapter06.pomodorotimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
