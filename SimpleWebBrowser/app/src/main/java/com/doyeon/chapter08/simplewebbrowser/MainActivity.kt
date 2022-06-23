8e655f538a3eff90c997ae8142b88c02441abb8e
doyeon326 <doyeon326@gmail.com>
2022-06-23 11:04:55 +0900

    feat: refreshView & progress Bar

--- SimpleWebBrowser/app/src/main/java/com/doyeon/chapter08/simplewebbrowser/MainActivity.kt
+++ SimpleWebBrowser/app/src/main/java/com/doyeon/chapter08/simplewebbrowser/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.doyeon.chapter08.simplewebbrowser
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.EditorInfo
+import android.webkit.URLUtil
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ProgressBar
+import androidx.core.widget.ContentLoadingProgressBar
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,6 +36,14 @@ class MainActivity : AppCompatActivity() {
 
     private val webView: WebView by lazy {
         findViewById(R.id.webView)
+    }
+
+    private val refreshLayout: SwipeRefreshLayout by lazy {
+        findViewById(R.id.refreshLayout)
+    }
+
+    private val progressBar: ContentLoadingProgressBar by lazy {
+        findViewById(R.id.progressBar)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,6 +68,7 @@ class MainActivity : AppCompatActivity() {
     private fun initViews() {
         webView.apply {
             webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
             settings.javaScriptEnabled = true
             loadUrl(DEFAULT_URL)
         }
@@ -62,8 +77,15 @@ class MainActivity : AppCompatActivity() {
     private fun bindViews() {
         addressBar.setOnEditorActionListener { v, actionId, event ->
             if(actionId == EditorInfo.IME_ACTION_DONE) {
-                webView.loadUrl(v.text.toString())
-                Log.d("MainActivity", "${v.text.toString()}")
+                val loadingUrl = v.text.toString()
+                if (URLUtil.isNetworkUrl(loadingUrl)) {
+                    //http or not
+                    webView.loadUrl(loadingUrl)
+                } else {
+                    webView.loadUrl("http://${loadingUrl}")
+                }
+               // webView.loadUrl(v.text.toString())
+               // Log.d("MainActivity", "${v.text.toString()}")
             }
             return@setOnEditorActionListener false
         }
@@ -78,6 +100,37 @@ class MainActivity : AppCompatActivity() {
 
         goHomeButton.setOnClickListener {
             webView.loadUrl(DEFAULT_URL)
+        }
+
+        refreshLayout.setOnRefreshListener {
+            webView.reload()
+        }
+    }
+
+    inner class WebViewClient: android.webkit.WebViewClient() {
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+            progressBar.show()
+        }
+
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            //로딩이 다 된 후
+            refreshLayout.isRefreshing = false
+            progressBar.hide()
+            goBackButton.isEnabled = webView.canGoBack()
+            goForwardButton.isEnabled = webView.canGoForward()
+            addressBar.setText(url) //최종적 로딩 url
+        }
+    }
+
+    inner class WebChromeClient: android.webkit.WebChromeClient() {
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+
+            progressBar.progress = newProgress
         }
     }
 
