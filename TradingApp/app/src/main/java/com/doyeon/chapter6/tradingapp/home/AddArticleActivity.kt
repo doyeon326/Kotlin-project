c36e59311e6ceb5a700eacfa3a1e901ff2c0832b
doyeon326 <doyeon326@gmail.com>
2022-07-07 20:12:15 +0900

    feat: photo uplosf

--- TradingApp/app/src/main/java/com/doyeon/chapter6/tradingapp/home/AddArticleActivity.kt
+++ TradingApp/app/src/main/java/com/doyeon/chapter6/tradingapp/home/AddArticleActivity.kt
@@ -5,15 +5,15 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.Toast
+import android.os.Handler
+import android.util.Log
+import android.widget.*
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.doyeon.chapter6.tradingapp.DBKey.Companion.DB_ARTICLES
 import com.doyeon.chapter6.tradingapp.R
 import com.google.firebase.auth.FirebaseAuth
@@ -24,6 +24,7 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 import java.net.URI
+import java.net.URL
 
 class AddArticleActivity: AppCompatActivity() {
     private var selectedUri: Uri? = null
@@ -59,7 +60,7 @@ class AddArticleActivity: AppCompatActivity() {
                     showPermisstionContextPopup()
                 }
                 else -> {
-                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), RESULT_OKAY)
+                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1010)
                 }
             }
         }
@@ -69,25 +70,64 @@ class AddArticleActivity: AppCompatActivity() {
 
             val sellerId = auth.currentUser?.uid.orEmpty()
 
-            val model = ArticleModel(sellerId, title, System.currentTimeMillis(), "$price 원", "")
-            articleDB.push().setValue(model)
-            finish()
+            showProgress()
+
+            //이미지가 있으면 업로드 과정을 추가
+            if (selectedUri != null) {
+                val photoUri = selectedUri ?: return@setOnClickListener
+                uploadPhoto(photoUri,
+                    successHandler = { uri ->
+                                     uploadArticle(sellerId, title, price, uri)
+                    },
+                    errorHandler = {
+                        hideProgress()
+                        Toast.makeText(this,"사진 업로드에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+
+                    )
+            } else {
+                uploadArticle(sellerId, title, price, "")
+            }
+
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-           1010 -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-               startContentProvider()
-            } else {
-                Toast.makeText(this, "권한을 거부하셨습니다", Toast.LENGTH_SHORT).show()
+    private fun uploadPhoto(uri: Uri, successHandler: (String) -> Unit, errorHandler: () -> Unit) {
+        val fileName = "${System.currentTimeMillis()}.png"
+        storage.reference.child("article/photo").child(fileName)
+            .putFile(uri)
+            .addOnCompleteListener{
+                if (it.isSuccessful) {
+                    storage.reference.child("article/photo").child(fileName)
+                        .downloadUrl
+                        .addOnSuccessListener { uri ->
+                            successHandler(uri.toString())
+                        }.addOnFailureListener{
+                            errorHandler()
+                        }
+                } else {
+                    errorHandler()
+                }
             }
+    }
 
+    private fun uploadArticle(sellerId: String, title:String, price: String, imageUrl: String) {
+        val model = ArticleModel(sellerId, title, System.currentTimeMillis(), "$price 원", imageUrl)
+        articleDB.push().setValue(model)
+        hideProgress()
+        finish()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            1010 ->
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startContentProvider()
+                } else {
+                    Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 
@@ -101,12 +141,9 @@ class AddArticleActivity: AppCompatActivity() {
 
     private fun getResultActivity() {
         getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            if (it.resultCode != Activity.RESULT_OK) {
-                return@registerForActivityResult
-            }
 
-            when (it.resultCode) {
-                RESULT_OKAY -> {
+                if(it.resultCode == Activity.RESULT_OK)  {
+                    Log.d("AddArticleActivity", "RESULT_OKAY:")
                     val data: Intent? = it.data
                     val uri = data?.data
 
@@ -116,10 +153,9 @@ class AddArticleActivity: AppCompatActivity() {
                     } else {
                         Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
                     }
-                } else -> {
+                } else {
                     Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
                 }
-            }
         }
     }
 
@@ -128,13 +164,17 @@ class AddArticleActivity: AppCompatActivity() {
             .setTitle("권한이 필요합니다.")
             .setMessage("사진을 가져오기 위해 필요합니다")
             .setPositiveButton("동의") { _, _ ->
-                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), RESULT_OKAY)
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1010)
             }
             .create()
             .show()
     }
 
-    companion object {
-        const val RESULT_OKAY = 2020
+    private fun showProgress() {
+        findViewById<ProgressBar>(R.id.progressBar).isVisible = true
     }
+    private fun hideProgress() {
+        findViewById<ProgressBar>(R.id.progressBar).isVisible = false
+    }
+
 }
