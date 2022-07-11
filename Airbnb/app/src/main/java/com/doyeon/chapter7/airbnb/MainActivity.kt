4d5df26aba81ad287e866179bdf25972860925d8
doyeon326 <doyeon326@gmail.com>
2022-07-11 16:56:26 +0900

    feat: get current location

--- Airbnb/app/src/main/java/com/doyeon/chapter7/airbnb/MainActivity.kt
+++ Airbnb/app/src/main/java/com/doyeon/chapter7/airbnb/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.doyeon.chapter7.airbnb
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.naver.maps.map.MapView
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.util.MarkerIcons
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
 
     private val mapView: MapView by lazy {
          findViewById(R.id.mapView)
@@ -16,6 +23,54 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+
+        naverMap.minZoom = 10.0
+        naverMap.maxZoom = 18.0
+
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.497885,127.027512))
+        naverMap.moveCamera(cameraUpdate)
+
+        val uiSetting = naverMap.uiSettings
+        uiSetting.isLocationButtonEnabled = true
+        //사용자의 위치 사용 권한을 받아야한다. -> manifest
+
+        locationSource = FusedLocationSource(this@MainActivity, LOCATION_PERMISSION_REQUEST_CODE)
+        naverMap.locationSource = locationSource
+
+        //마커 찍기
+        val marker = Marker()
+        marker.position = LatLng(37.4049888,126.6281074 )
+        marker.map = naverMap
+        marker.icon = MarkerIcons.BLACK
+        marker.iconTintColor = Color.RED
+            /*
+            *
+            * */
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return
+        }
+
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) {
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
     }
 
     override fun onStart() {
@@ -51,5 +106,9 @@ class MainActivity : AppCompatActivity() {
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
