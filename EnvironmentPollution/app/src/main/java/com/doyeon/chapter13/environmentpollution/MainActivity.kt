bab7b1182315f0cc8331162776bda62725031aaa
doyeon326 <doyeon326@gmail.com>
2022-08-01 14:09:41 +0900

    feat: layout added

--- EnvironmentPollution/app/src/main/java/com/doyeon/chapter13/environmentpollution/MainActivity.kt
+++ EnvironmentPollution/app/src/main/java/com/doyeon/chapter13/environmentpollution/MainActivity.kt
@@ -5,8 +5,13 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.app.ActivityCompat
+import androidx.core.view.isVisible
 import com.doyeon.chapter13.environmentpollution.data.Repository
+import com.doyeon.chapter13.environmentpollution.data.models.airquality.Grade
+import com.doyeon.chapter13.environmentpollution.data.models.airquality.MeasuredValue
+import com.doyeon.chapter13.environmentpollution.data.models.monitoringstation.MonitoringStation
 import com.doyeon.chapter13.environmentpollution.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
@@ -16,6 +21,7 @@ import com.google.android.gms.tasks.CancellationTokenSource
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,6 +40,7 @@ class MainActivity : AppCompatActivity() {
 
         initVariables()
         requestLocationPermissions()
+        bindViews()
     }
 
     override fun onDestroy() {
@@ -61,13 +68,64 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun displayAirQualityData(monitoringStation: MonitoringStation, measuredValue: MeasuredValue) {
+        binding.contentsLayout.animate()
+            .alpha(1F)
+            .start()
+
+        binding.measuringStationName.text = monitoringStation.stationName
+        binding.measuringStationAddressTextView.text = monitoringStation.addr
+
+        (measuredValue.khaiGrade ?: Grade.UNKNOWN).let { grade ->
+            binding.root.setBackgroundResource(grade.colorResId)
+            binding.totalGradeEmojiTextView.text = grade.emoji
+            binding.totalGradeLabelTextView.text = grade.label
+        }
+
+
+        with(measuredValue) {
+            binding.findDustInformationTextView.text =
+                "미세먼지: $pm10Value ㎍/㎥ ${(pm10Grade ?: Grade.UNKNOWN).emoji}"
+            binding.ultraFindDustInformation2TextView.text =
+                "초미세먼지: $pm25Value ㎍/㎥ ${(pm25Grade ?: Grade.UNKNOWN).emoji}"
+
+            with(binding.so2Item) {
+                labelTextView.text = "아황산가스"
+                gradeTextView.text = (so2Grade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$so2Value ppm"
+            }
+
+            with(binding.coItem) {
+                labelTextView.text = "일산화탄소"
+                gradeTextView.text = (coGrade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$so2Value ppm"
+            }
+
+            with(binding.o3Item) {
+                labelTextView.text = "오존"
+                gradeTextView.text = (o3Grade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$o3Value ppm"
+            }
+
+            with(binding.no2Item) {
+                labelTextView.text = "이산화질소"
+                gradeTextView.text = (no2Grade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$no2Value ppm"
+            }
+
+        }
+
+
+
+    }
+
     private fun requestLocationPermissions() {
         ActivityCompat.requestPermissions(
             this,
             arrayOf(
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION
-                ),
+            ),
             REQUEST_ACCESS_LOCATION_PERMISSIONS
         )
     }
@@ -78,24 +136,43 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("MissingPermission")
     private fun fetchAirQualityData() {
+
         //fetching data
         cancellationTokenSource = CancellationTokenSource()
         fusedLocationProviderClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY,
             cancellationTokenSource!!.token
         ).addOnSuccessListener { location ->
-            binding.textView.text = "${location.latitude}, ${location.longitude}"
+            //binding.textView.text = "${location.latitude}, ${location.longitude}"
 
             scope.launch {
                 //coroutineScope 시작
-                val monitoringStation =
-                    Repository.getNearbyMonitoringStation(location.latitude, location.longitude)
+                binding.errorDescriptionTextView.visibility = View.GONE
+                try {
+                    val monitoringStation =
+                        Repository.getNearbyMonitoringStation(location.latitude, location.longitude)
 
-                val measuredValue = Repository.getLatestAirQualityData(monitoringStation!!.stationName!! )
-                 binding.textView.text = measuredValue.toString()
+                    val measuredValue =
+                        Repository.getLatestAirQualityData(monitoringStation!!.stationName!!)
+                    //binding.textView.text = measuredValue.toString()
+
+                    displayAirQualityData(monitoringStation, measuredValue!!)
+                }catch (exception: Exception) {
+                    binding.errorDescriptionTextView.visibility = View.VISIBLE
+                    binding.contentsLayout.alpha = 0F
+                } finally {
+                    binding.progressBar.visibility = View.GONE
+                    binding.refresh.isRefreshing = false
+                 }
             }
         }
     }
 
+
+    private fun bindViews() {
+        binding.refresh.setOnRefreshListener {
+            fetchAirQualityData()
+        }
+    }
     companion object {
         private const val REQUEST_ACCESS_LOCATION_PERMISSIONS = 100
     }
