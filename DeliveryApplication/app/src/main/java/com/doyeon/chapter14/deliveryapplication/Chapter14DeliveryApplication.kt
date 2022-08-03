cfd11a98bf8d0e6a074041b4c1212a47f8acae86
doyeon326 <doyeon326@gmail.com>
2022-08-03 13:00:19 +0900

    setup: Koin DI

--- DeliveryApplication/app/src/main/java/com/doyeon/chapter14/deliveryapplication/Chapter14DeliveryApplication.kt
+++ DeliveryApplication/app/src/main/java/com/doyeon/chapter14/deliveryapplication/Chapter14DeliveryApplication.kt
@@ -2,12 +2,17 @@ package com.doyeon.chapter14.deliveryapplication
 
 import android.app.Application
 import android.content.Context
+import com.doyeon.chapter14.deliveryapplication.di.appModule
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 class Chapter14DeliveryApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
         appContext = this
+
+        startKoin { modules(appModule) }
     }
 
     override fun onTerminate() {
---
+++ DeliveryApplication/app/src/main/java/com/doyeon/chapter14/deliveryapplication/di/appModule.kt
@@ -0,0 +1,21 @@
+package com.doyeon.chapter14.deliveryapplication.di
+
+import com.doyeon.chapter14.deliveryapplication.util.provider.DefaultResourcesProvider
+import com.doyeon.chapter14.deliveryapplication.util.provider.ResourcesProvider
+import kotlinx.coroutines.Dispatchers
+import org.koin.android.ext.koin.androidApplication
+import org.koin.dsl.module
+
+val appModule = module {
+    //koin DI 셋업
+
+    single { provideGsonConvertFactory() }
+    single { buildOkHttpClient() }
+
+    single { provideRetrofit(get(), get()) }
+    single<ResourcesProvider> { DefaultResourcesProvider(androidApplication()) }
+
+    single { Dispatchers.IO }
+    single { Dispatchers.Main }
+
+}
